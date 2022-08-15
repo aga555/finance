52094d74bc8e74900f212787de914df531d91582
akrawczyk <agammkrawczyk@gmail.com>
2022-08-15 22:53:25 +0200

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyCTylHvAh2u-mmPKR_AtQ37f0Uo7-gfpog',
+    authDomain: 'angular-leads.firebaseapp.com',
+    databaseURL: 'https://angular-leads-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'angular-leads',
+    storageBucket: 'angular-leads.appspot.com',
+    messagingSenderId: '148034085566',
+    appId: '1:148034085566:web:e4760d1ab85350055934ef',
+    measurementId: 'G-S2B2L0NPP4'
+  }
 };
